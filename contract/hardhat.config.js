b2db8f92b84b2f4504b8d9f9753dfdefcdd87c97
akp111 <pradhanashish95@gmail.com>
2022-08-21 21:23:54 +0530

    changes

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -26,7 +26,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
     hardhat: {
       forking: {
-        url: "https://polygon-mumbai.g.alchemy.com/v2/U7-rQDtPo9P6mDi_gNjQW761_j_nfk8P"
+        url: process.env.MUMBAI_URL
       },
       gas: 12000000,
       blockGasLimit: 0x1fffffffffffff,
@@ -34,9 +34,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
       timeout: 1800000
     },
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/b_hn2oETygzJLSLi4Bt69dDZVzPz5P-l" || "",
+      url:  process.env.MUMBAI_URL || "",
       accounts:
-        ["0x4967d9360f24a79a6b0e7eae94c9460ef00039a7d04f89defcb07ef7f4ec80ad"],
+        [process.env.PRIVATE_KEY],
       allowUnlimitedContractSize: true,
     },
   },
--- contract/scripts/test.js
+++ contract/scripts/test.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const ethers = require("ethers");
-const privateKey =
-  "0x4967d9360f24a79a6b0e7eae94c9460ef00039a7d04f89defcb07ef7f4ec80ad";
+const privateKey = process.env.PRIVATE_KEY;
 const contractAddress = "0x5e7fE8E7243925B4e239fc0B93617Fe4903cA2f8";
 const contractABI = require("./abi.json");
 const signature =
   "0x4a3e0bf1f4bf926f99bda0bfa0d031c88e198969510c617651012237529922ea7d29b1fe890daf96e081640d889575429a8881b368def4f8ab33480197b6e8ad1b";
-const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/b_hn2oETygzJLSLi4Bt69dDZVzPz5P-l");
+const provider = new ethers.providers.JsonRpcProvider(process.env.MUMBAI_URL);
 const wallet = new ethers.Wallet(privateKey, provider);
 const newContract = new ethers.Contract(contractAddress, contractABI, provider);
 const signingContract = newContract.connect(wallet);
